e15e8d7912a89828ad1c3b71c3bc6f2dff7dbb45
Hessel Bonenkamp <hessel@mediamonks.com>
2020-05-11 12:10:39 +0200

    Added basic in-editor downloading

--- Runtime/BackgroundDownloadEditor.cs
+++ Runtime/BackgroundDownloadEditor.cs
@@ -1,31 +1,69 @@
 #if UNITY_EDITOR
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using UnityEngine;
 
 namespace Unity.Networking
 {
-    class BackgroundDownloadEditor : BackgroundDownload
-    {
-        public BackgroundDownloadEditor(BackgroundDownloadConfig config)
-            : base(config)
-        {
-            _status = BackgroundDownloadStatus.Failed;
-            _error = "Not implemented for Unity Editor";
-        }
+	class BackgroundDownloadEditor : BackgroundDownload
+	{
+		static HttpClient _client;
+		float _progress;
 
-        public override bool keepWaiting { get { return false; } }
+		[RuntimeInitializeOnLoadMethod]
+		static void Init()
+		{
+			_client = new HttpClient();
+		}
 
-        protected override float GetProgress() { return 1.0f; }
+		public BackgroundDownloadEditor(BackgroundDownloadConfig config)
+			: base(config)
+		{
+			Start(config);
+		}
 
-        internal static Dictionary<string, BackgroundDownload> LoadDownloads()
-        {
-            return new Dictionary<string, BackgroundDownload>();
-        }
+		private async void Start(BackgroundDownloadConfig config)
+		{
+			_status = BackgroundDownloadStatus.Downloading;
+			_error = "";
+			_progress = 0f;
 
-        internal static void SaveDownloads(Dictionary<string, BackgroundDownload> downloads)
-        {
-        }
-    }
+			var persistentFilePath = Path.Combine(_persistentDataPath, config.filePath);
+			try
+			{
+				using (var str = await _client.GetStreamAsync(_config.url))
+				using (var stream = new FileStream(persistentFilePath, FileMode.Create, FileAccess.Write, FileShare.None))
+				{
+					await str.CopyToAsync(stream);
+				}
+
+				_progress = 1f;
+				_status = BackgroundDownloadStatus.Done;
+				_error = "";
+			}
+			catch (Exception e)
+			{
+				_error = $"{e.GetType()} during download: {e.Message}";
+				_status = BackgroundDownloadStatus.Failed;
+			}
+		}
+
+		public override bool keepWaiting => _status == BackgroundDownloadStatus.Downloading;
+
+		protected override float GetProgress() => _progress;
+
+		internal static Dictionary<string, BackgroundDownload> LoadDownloads()
+		{
+			return new Dictionary<string, BackgroundDownload>();
+		}
+
+		internal static void SaveDownloads(Dictionary<string, BackgroundDownload> downloads) { }
+
+		public override void Dispose() { base.Dispose(); }
+	}
 }
 
 #endif
